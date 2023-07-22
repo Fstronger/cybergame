afe5c8bc342705fabcb79326358a18885007bd1a
Vladimir <diablo_96_96@mail.ru>
2023-07-23 01:05:12 +0600

    CYBER-10 Предварительная трехуровненая регистрация

---
+++ resources/js/Pages/Auth/Partials/RegisterStepOne.vue
@@ -0,0 +1,92 @@
+<script setup>
+import InputError from '@/Components/InputError.vue';
+import InputLabel from '@/Components/InputLabel.vue';
+import TextInput from '@/Components/TextInput.vue';
+import {Link, useForm} from '@inertiajs/vue3';
+import { ref } from 'vue';
+
+const passwordInput = ref(null);
+const currentPasswordInput = ref(null);
+
+const form = useForm({
+    current_password: '',
+    password: '',
+    password_confirmation: '',
+});
+
+</script>
+
+<template>
+    <section>
+        <header>
+            <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">Первый шаг регистрации</h2>
+
+            <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+                Заполните уникальное имя пользователя, почту, пароль и его подтверждение
+            </p>
+        </header>
+
+        <div class="step-one">
+            <div>
+                <InputLabel for="name" value="Name" />
+
+                <TextInput
+                    id="name"
+                    type="text"
+                    class="mt-1 block w-full"
+                    v-model="form.name"
+                    required
+                    autofocus
+                    autocomplete="name"
+                />
+
+                <InputError class="mt-2" :message="form.errors.name" />
+            </div>
+
+            <div class="mt-4">
+                <InputLabel for="email" value="Email" />
+
+                <TextInput
+                    id="email"
+                    type="email"
+                    class="mt-1 block w-full"
+                    v-model="form.email"
+                    required
+                    autocomplete="username"
+                />
+
+                <InputError class="mt-2" :message="form.errors.email" />
+            </div>
+
+            <div class="mt-4">
+                <InputLabel for="password" value="Password" />
+
+                <TextInput
+                    id="password"
+                    type="password"
+                    class="mt-1 block w-full"
+                    v-model="form.password"
+                    required
+                    autocomplete="new-password"
+                />
+
+                <InputError class="mt-2" :message="form.errors.password" />
+            </div>
+
+            <div class="mt-4">
+                <InputLabel for="password_confirmation" value="Confirm Password" />
+
+                <TextInput
+                    id="password_confirmation"
+                    type="password"
+                    class="mt-1 block w-full"
+                    v-model="form.password_confirmation"
+                    required
+                    autocomplete="new-password"
+                />
+
+                <InputError class="mt-2" :message="form.errors.password_confirmation" />
+            </div>
+        </div>
+    </section>
+</template>
---
+++ resources/js/Pages/Auth/Partials/RegisterStepThree.vue
@@ -0,0 +1,38 @@
+<script setup>
+
+import SecondaryButton from '@/Components/SecondaryButton.vue';
+
+defineProps({ heroes: Object })
+
+</script>
+
+<template>
+    <section>
+        <header>
+            <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">Третий шаг регистрации</h2>
+
+            <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+                Выберите фракцию
+            </p>
+        </header>
+
+        <div class="step-two">
+            <ul class="p-6 text-gray-900 dark:text-gray-100" id="example-1">
+                <li v-for="hero in heroes.data" :key="hero.id">
+                    <br>
+                    {{ hero.name }}
+                    <br>
+                    <br>
+                    {{ hero.description }}
+                    <li v-for="char in hero.characteristics" :key="hero.id">
+                        <br>
+                        {{ char.name }}
+                        <br>
+                        <br>
+                        {{ char.amount }}
+                    </li>
+                </li>
+            </ul>
+        </div>
+    </section>
+</template>
