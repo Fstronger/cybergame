035c5bc56a08285d90ce986f086098fabfe77e09
Vladimir <diablo_96_96@mail.ru>
2023-07-22 16:27:17 +0600

    Первоначальнай хуйня База с персонажом характеристиками и фракциями с героями

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -17,7 +17,7 @@ defineProps({ factions: Object })
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900 dark:text-gray-100">Вы успешно авторизованы</div>
+                    <div class="p-6 text-gray-900 dark:text-gray-100">Информация о фракциях</div>
 
                     <ul class="p-6 text-gray-900 dark:text-gray-100" id="example-1">
                         <li v-for="faction in factions" :key="faction.id">
