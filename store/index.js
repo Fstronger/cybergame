ef541f5613913f21227cfed3fa2a1c0e97a8f64a
Tony Klimenkov <plootony.dev@gmail.com>
2023-07-27 00:43:08 +0500

    CYBER-17: Написать vue store api для взаимодействия с бд
    Фракции подгрузаются из бд
    
    Персонажи подгружаются из бд

--- store/index.js
+++ store/index.js
@@ -1,4 +1,5 @@
-import { createStore } from 'vuex';
+import {createStore} from 'vuex';
+import axios from 'axios';
 
 export default createStore({
     state: {
@@ -18,23 +19,98 @@ export default createStore({
                 name: ''
             },
         },
+        factions: null,
+        heroes: null,
     },
     mutations: {
+        /**
+         * Получаем фракции из бд
+         *
+         * @param state
+         * @param payload
+         * @constructor
+         */
+        GET_FACTIONS(state, payload) {
+            state.factions = payload;
+        },
+
+        /**
+         * Получаем персонажей из бд
+         *
+         * @param state
+         * @param payload
+         * @constructor
+         */
+        GET_HEROES(state, payload) {
+            state.heroes = payload;
+        },
+
+        /**
+         * Забираем авторизационные данные из формы регистрации
+         *
+         * @param state
+         * @param authData
+         * @constructor
+         */
         SET_AUTH(state, authData) {
             state.registration.auth = authData;
         },
+
+        /**
+         * Забираем выбранную фракцию
+         *
+         * @param state
+         * @param faction
+         * @constructor
+         */
         SET_FACTION(state, faction) {
             state.registration.faction = faction;
         },
+
+        /**
+         * Забираем выбранного персонажа
+         *
+         * @param state
+         * @param character
+         * @constructor
+         */
         SET_CHARACTER(state, character) {
             state.registration.character = character;
         },
+
+        /**
+         * Обнуление стора регистрации, хз может пригодится
+         *
+         * @param state
+         * @constructor
+         */
         RESET_REGISTRATION(state) {
             state.registration = {
                 authData: '',
                 faction: '',
                 character: '',
             };
+        },
+    },
+    actions: {
+        // Фетчим фракции из бд
+        async fetchFactions({commit}) {
+            try {
+                const response = await axios.get('/factions'); // Здесь замените URL на ваш API
+                commit('GET_FACTIONS', response.data);
+            } catch (error) {
+                console.error('Ошибка при выполнении запроса:', error);
+            }
+        },
+
+        // Фетчим фракции из бд
+        async fetchHeroes({commit}) {
+            try {
+                const response = await axios.get(`/faction-heroes/1`);
+                commit('GET_HEROES', response.data);
+            } catch (error) {
+                console.error('Ошибка при выполнении запроса:', error);
+            }
         },
     },
 });
